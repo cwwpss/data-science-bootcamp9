00fc8ba6e52e4da0a7df1d74dffa0299cf051265
Chawalwit Phosai <154348025+cwwpss@users.noreply.github.com>
2024-04-05 18:10:53 +0700

    Update live_class_data_trans.r

--- Data Transformation with R/live_class_data_trans.r
+++ Data Transformation with R/live_class_data_trans.r
@@ -4,3 +4,11 @@
 install.package(tidyverse)
 library(tidyverse)
 
+## Glue library => glue message same paste()
+my_name <- "toy"
+my_age <- 35
+
+glue("Hi! myname is {my_name}. Today I'm {my_age} years old.")
+paste0("Hello my name is ", my_name, ".")
+
+
