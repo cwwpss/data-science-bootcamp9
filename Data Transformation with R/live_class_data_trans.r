4191a056358a836e6bf471b79b8118be8a158945
Chawalwit Phosai <154348025+cwwpss@users.noreply.github.com>
2024-04-05 16:11:33 +0700

    Update live_class_data_trans.r

--- Data Transformation with R/live_class_data_trans.r
+++ Data Transformation with R/live_class_data_trans.r
@@ -1,1 +1,6 @@
+# Data Transformation with R
+## library in R => tidyverse, glue, RSQlite, lubridate
+## use function install.package() or in package panel
+install.package(tidyverse)
+library(tidyverse)
 
